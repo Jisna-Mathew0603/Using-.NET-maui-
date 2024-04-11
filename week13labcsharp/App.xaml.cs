e8fbf086d5c3fe6e32db17e87c891c17cfe6ae2f
JISNA <JISNA@LAPTOP-METFMLOC>
2024-04-10 23:47:11 -0400

    Add project files.

---
+++ week13labcsharp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace week13labcsharp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ week13labcsharp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace week13labcsharp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
